a778e9fea1677ced2e7650154cc40469618e285c
hassan hamdan <hassanhamdan01@lau.edu>
2023-01-27 20:29:17 +0200

    initial info provided

---
+++ Profile.java
@@ -0,0 +1,10 @@
+public class Profile {
+	public static void main(String[] args) {
+		System.out.println("Hassan Fouad Hamdan");
+		System.out.println("Mechatronic engineering");
+		System.out.println("2023-2024");
+
+
+
+	}
+}
